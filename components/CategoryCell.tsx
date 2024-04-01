5df43c079403e37fb0fd91d3db477fe435ba854b
luc.le <luc.le@enouvo.com>
2024-04-01 20:38:22 +0700

    description

--- components/CategoryCell.tsx
+++ components/CategoryCell.tsx
@@ -8,13 +8,18 @@ import { cn } from "@/utils/cn.util";
 
 type CategoryCellProps = Category;
 
-export function CategoryCell({ id, name, thumbnail, span }: CategoryCellProps) {
+export function CategoryCell({ id, name, thumbnail, span = 1 }: CategoryCellProps) {
   return (
     <Link
       href={`/categories/${id.toString()}`}
-      className={cn("col-span-2 flex h-cell w-2cell overflow-hidden rounded-2xl bg-white", {
-        "h-2cell flex-col": span === 2,
-      })}
+      className={cn(
+        "col-span-2 flex grid-flow-dense overflow-hidden rounded-2xl bg-white transition-all",
+        "hover:scale-105",
+        {
+          "size-2cell flex-col row-span-2": span === 2,
+          "h-cell w-2cell": span === 1,
+        }
+      )}
     >
       {name === POKI_KIDS_NAME ? (
         <div className="flex size-full items-center justify-center bg-yellow-5 p-4">
