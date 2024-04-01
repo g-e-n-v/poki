1653c44ea688fc4f86ed63b0bbf8a2cc702c2c7d
luc.le <luc.le@enouvo.com>
2024-04-01 23:24:10 +0700

    add example

--- app/[lang]/[slug]/page.tsx
+++ app/[lang]/[slug]/page.tsx
@@ -5,13 +5,15 @@ import { GamePlay } from "@/components/GamePlay";
 import { Navigation } from "@/components/Navigation";
 import { getGameDetail } from "@/services/get-game-detail.service";
 import { getGames } from "@/services/get-games.service";
+import { type Locale } from "@/translate/translator";
 import { cn } from "@/utils/cn.util";
 
 type GameDetailPageProps = {
-  params: { slug: string };
+  params: { slug: string; lang: Locale };
 };
 
 export default async function GameDetailPage({ params }: GameDetailPageProps) {
+  // const t = translator[params.lang];
   const games = await getGames();
   const currentGame = await getGameDetail(params.slug);
 
@@ -23,6 +25,8 @@ export default async function GameDetailPage({ params }: GameDetailPageProps) {
     <div className="mx-auto w-max">
       <div className={cn("grid-container pt-4")}>
         <Navigation />
+
+        {/* <div>{t.About}</div> */}
 
         <GamePlay
           className={cn("col-span-6 col-start-2 row-span-4", "11c:col-span-8 11c:row-span-5")}
